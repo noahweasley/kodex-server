4febacfde8433bf3080d4b0ce2144165906111d3
noahweasley <iebenmelu@gmail.com>
2025-03-18 14:37:41 +0100

    change to my name

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 const port = 3000;
 
 app.get("/", (_req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Noah!");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on ${port}`);
 });
