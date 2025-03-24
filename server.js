aa346671603846e8947eeb73f5881f81b5374bc1
noahweasley <iebenmelu@gmail.com>
2025-03-24 14:41:30 +0100

    deploy

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
 
 app.get("/", (_req, res) => {
-  res.send("Hello Noah!");
+  res.send(process.env.NODE_ENV);
+  console.log(process.env.NODE_ENV)
 });
 
 app.listen(port, () => {
